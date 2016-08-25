a49ba69f8587e0e34a756f0566f8fabe1eb7d2bb
Domakonda <adomakon@amazon.com>
2016-08-24 23:56:08 -0700

    Added db

--- src/com/attendance/portal/AppConfig.java
+++ src/com/attendance/portal/AppConfig.java
@@ -1,7 +1,12 @@
 package com.attendance.portal;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import com.google.gson.Gson;
 
@@ -11,6 +16,20 @@ public class AppConfig {
 	@Bean 
 	public Gson testGson(){
 		System.out.println("\n\n\n\n\n\n\n\n Gson Bean Created !!! \n\n\n\n\n\n\n\n");
-	   return new Gson();
+		return new Gson();
+	}
+	
+	@Bean
+	public DataSource dataSource() {
+		// no need shutdown, EmbeddedDatabaseFactoryBean will take care of this
+		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+		EmbeddedDatabase db = builder
+			.setType(EmbeddedDatabaseType.HSQL) //.H2 or .DERBY
+			//.addScript("db/sql/create-db.sql") //Add these later when you have your schema figured out raveena
+			//.addScript("db/sql/insert-data.sql")
+			.build();
+		System.out.println("\n\n\n\n\n\n\n\n DB Bean Created !!! \n\n\n\n\n\n\n\n");
+
+		return db;
 	}
 }
