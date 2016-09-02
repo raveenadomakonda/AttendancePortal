df15abcdd3aba4c2a0589a45e84de41fce633845
raveena <raveenadk@gmail.com>
2016-09-02 13:12:38 -0400

    made create tables private

--- src/com/attendance/portal/Faculty.java
+++ src/com/attendance/portal/Faculty.java
@@ -1,5 +1,23 @@
 package com.attendance.portal;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 public class Faculty {
-	
+	public static int addFaculty(DataSource db, int facultyId, String lastName, String firstName, String phone) throws SQLException{
+		Connection conn = db.getConnection();
+		String s = "INSERT INTO Faculty VALUES(?,?,?,?)";
+		PreparedStatement p = conn.prepareStatement(s);
+		p.setInt(1, facultyId);
+		p.setString(2,lastName);
+		p.setString(3,firstName);
+		p.setString(4, phone);
+		return p.executeUpdate();
+		
+		
+	}
 }
